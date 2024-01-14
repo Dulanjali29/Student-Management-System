ec33df46e2b8ceeeaa3b3c022b5a054877cdfc45
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-01-14 15:30:42 +0530

    create-ui-signin

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Home from '../pages/Home/Home'
 import './App.css'
+import SignUp from '../pages/SignUp/SignUp'
+import Signin from '../pages/Signin/Sigin'
+import { Box } from '@mui/material'
 
 function App() {
  
 
   return (
 <div>
-<Home/>
+  {/* <Box sx={{display:'flex',justifyContent:'center'}}>
+    <SignUp/>
+  </Box> */}
+  <Box sx={{display:'flex',justifyContent:'center'}}>
+    <Signin/>
+  </Box>
 </div>
 
   )
--- src/pages/Signin/Sigin.jsx
+++ src/pages/Signin/Sigin.jsx
@@ -3,7 +3,7 @@ import { TextField } from "@mui/material";
 import Card from "@mui/material/Card";
 import Button from '@mui/material/Button';
 import Box from "@mui/material/Box";
-export default function SignUp(){
+export default function Signin(){
     return(
         <div>
              <Card sx={{minWidth:300,maxWidth:350,margin:2,padding:1}}   >
@@ -12,25 +12,19 @@ export default function SignUp(){
                 sx={{margin:1}}
                 fullWidth
                  id="outlined-basic"
-                  label="Name *" 
+                  label="E mail *" 
                   variant="outlined" 
                   />
                  <TextField
                 sx={{margin:1}}
                 fullWidth
                  id="outlined-basic"
-                  label="E mail * " 
-                  variant="outlined" 
-                  />
-                  <TextField
-                sx={{margin:1}}
-                fullWidth
-                 id="outlined-basic"
                   label="Password * " 
                   variant="outlined" 
                   />
+               
                   <Box sx={{display:'flex',justifyContent:'center'}}>
-                  <Button variant="contained" fullWidth>SIGN UP</Button>
+                  <Button variant="contained" fullWidth>SIGNIN</Button>
                   </Box>
              </Card>
         </div>
