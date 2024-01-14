3577cac6a3cc7a5b548832cec3397542c71022ac
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-01-14 08:09:44 +0530

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SignUp from '../pages/SignUp/SignUp'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+<div>Student Management System
+<SignUp/>
+</div>
+
+  )
+}
+
+export default App
