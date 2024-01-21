c9f377965a18452794adc16a9cc915dd1c2d07a3
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-01-21 19:38:02 +0530

    routing signup and signin page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Dashboard from '../pages/Dashboard/Dashboard'
 import './App.css'
 import SignUp from '../pages/SignUp/SignUp'
-import Signin from '../pages/Signin/Sigin'
+import SignIn from '../pages/SignIn/SignIn'
 import  Box  from '@mui/material/Box';
+import {Route,Routes,Link,Navigate} from 'react-router-dom'
 
-
+import Main from '../component/Main/Main'
 
 function App() {
  
-
+const [signin,setSignin]=useState(false);
   return (
 <div>
-  {/* <Box sx={{display:'flex',justifyContent:'center',marginTop:5}}>
-    <SignUp/>
-  </Box> */}
-  {/* <Box sx={{display:'flex',justifyContent:'center'}}>
-    <Signin/>
-  </Box> */}
-  <Dashboard/>
+  {
+    signin ?
+    <Dashboard/>
+    :
+    <Box sx={{display:'flex',justifyContent:'center',marginTop:5}}>
+    <Routes>
+    <Route path='*' element={< Navigate to ={'/signin'} />}/>
+      <Route path={'/signin'} element={<SignIn/>}/>
+      <Route path={'/signup'} element={<SignUp/>}/>
+    </Routes>
+    </Box>
+  }
+
+
+  
+
 </div>
 
   )
